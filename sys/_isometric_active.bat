1703312a231345378ffb0b5d680df896dce7a065
MythMega <mythmegass@gmail.com>
2023-07-02 04:12:20 +0200

    new update -fix lots of things

---
+++ sys/_isometric_active.bat
@@ -0,0 +1,2 @@
+ren isometric-renders-0.4.4+1.20.jard isometric-renders-0.4.4+1.20.jar
+ren owo-lib-0.11.0+1.20.jard owo-lib-0.11.0+1.20.jar
---
+++ sys/_isometric_desactive.bat
@@ -0,0 +1,2 @@
+ren isometric-renders-0.4.4+1.20.jar isometric-renders-0.4.4+1.20.jard
+ren owo-lib-0.11.0+1.20.jar owo-lib-0.11.0+1.20.jard
